beb1e33b0b661da23b068234b1e621c88a927617
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-18 02:05:54 +0500

    Task6

---
+++ Tyuiu.UrlaherKA.Sprint2.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.UrlaherKA.Sprint2.Task6.V11.Lib
+{
+    public class DataService : ISprint2Task6V11
+    {
+        public string FindDateOfNextDay(int g, int m, int n)
+        {
+            String res;
+            if (m > 12 | m <= 0 | n > 31 | n <= 0) return res = "Неправильно введён месяц или день";
+            else
+            {
+                switch (m, n)
+                {
+                    case (1, 31): return res = $"{g},{m+1},{n = 1}";
+                    case (2, 29): return res = $"{g},{m + 1},{n =1}";
+                    case (3, 31): return res = $"{g},{m + 1},{n =1}";
+                    case (4, 30): return res = $"{g},{m + 1},{n =1}";
+                    case (5, 31): return res = $"{g},{m + 1},{n =1}";
+                    case (6, 30): return res = $"{g},{m + 1},{n =1}";
+                    case (7, 31): return res = $"{g},{m + 1},{n =1}";
+                    case (8, 31): return res = $"{g},{m + 1},{n =1}";
+                    case (9, 30): return res = $"{g},{m + 1},{n =1}";
+                    case (10, 31): return res = $"{g},{m + 1},{n =1}";
+                    case (11, 30): return res = $"{g},{m + 1},{n =1}";
+                    case (12, 31): return res = $"{g+=1},{m =1},{n =1}"; 
+                    default: return res = $"{g},{m},{n += 1}";
+                }
+            }
+        }
+    }
+}
---
+++ Tyuiu.UrlaherKA.Sprint2.Task6.V11/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Tyuiu.UrlaherKA.Sprint2.Task6.V11.Lib;
+namespace Tyuiu.UrlaherKA.Sprint2.Task5.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Урлахер К. А. | ПКТб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Урлахер Кирилл Александрович | ПКТб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите год");
+            int g = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите день");
+            int n = int.Parse(Console.ReadLine());
+
+            string res = ds.FindDateOfNextDay(g, m, n);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res == "Неправильно введён месяц или день") Console.WriteLine("Неправильно введён месяц или день");
+            else Console.WriteLine($"Следующий день будет: {res}");
+
+        }
+    }
+}
+
