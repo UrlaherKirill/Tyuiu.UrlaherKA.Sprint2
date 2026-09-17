bacb0244300c4c17a349229e218c2d1446a75fbf
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-17 22:55:36 +0500

    Task5

---
+++ Tyuiu.UrlaherKA.Sprint2.Task5.V13.Lib/DataService.cs
@@ -0,0 +1,75 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.UrlaherKA.Sprint2.Task5.V13.Lib
+{
+    public class DataService : ISprint2Task5V13
+    {
+        public string FindDateOfNextDay(int g, int m, int n)
+        {
+            String res;
+            if (m > 12 | m <= 0 | n > 31 | n <= 0) return res = "Неправильно введён месяц или день";
+            else
+            {
+
+            
+                switch (m, n)
+                {
+                    case (1, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (2, 29):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (3, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (4, 30):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (5, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (6, 30):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (7, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (8, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (9, 30):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (10, 31):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (11, 30):
+                        m += 1;
+                        n = 1;
+                        break;
+                    case (12, 31):
+                        m = 1;
+                        n = 1;
+                        g += 1;
+                        break;
+                    default:
+                        n += 1;
+                        break;
+                }
+                res = $"{g},{m},{n}";
+                return res;
+            }
+    
+        }       
+    }
+}
---
+++ Tyuiu.UrlaherKA.Sprint2.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.UrlaherKA.Sprint2.Task5.V13.Lib;
+namespace Tyuiu.UrlaherKA.Sprint2.Task5.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void Default_Day()
+        {
+            int g, m, n;
+            g = 1944;
+            m = 10;
+            n = 15;
+            string res = ds.FindDateOfNextDay(g, m, n);
+            string Wait_res = "1944,10,16";
+            Assert.AreEqual(res, Wait_res);
+        }
+        [TestMethod]
+        public void Next_Month()
+        {
+            int g, m, n;
+            g = 1944;
+            m = 10;
+            n = 31;
+            string res = ds.FindDateOfNextDay(g, m, n);
+            string Wait_res = "1944,11,1";
+            Assert.AreEqual(res, Wait_res);
+        }
+        [TestMethod]
+        public void Next_year()
+        {
+            int g, m, n;
+            g = 1944;
+            m = 12;
+            n = 31;
+            string res = ds.FindDateOfNextDay(g, m, n);
+            string Wait_res = "1945,1,1";
+            Assert.AreEqual(res, Wait_res);
+
+        }
+        [TestMethod]
+        public void Error_Day_Month()
+        {
+            int g, m, n;
+            g = 1944;
+            m = 13;
+            n = 40;
+            string res = ds.FindDateOfNextDay(g, m, n);
+            string Wait_res = "Неправильно введён месяц или день";
+            Assert.AreEqual(res, Wait_res);
+        }
+    }
+}
